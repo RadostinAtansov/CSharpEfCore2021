e51c5428263c1bbc7a6c077911b8e59c7c648916
Radostin Atanasov <16541785+RadostinAtansov@users.noreply.github.com>
2021-04-27 20:42:29 +0300

    Add files via upload

---
+++ FastFood/FastFood.Data/FastFoodYourDbContextDesignTimeFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastFood.Data
+{
+    public class FastFoodYourDbContextDesignTimeFactory : IDesignTimeDbContextFactory<FastFoodContext>
+    {
+
+        public FastFoodContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<FastFoodContext>();
+
+            //Configuration.GetDirect --> drug nachi za vzemane na connection string
+            //TODO: Configuration.GetConnectionString("DefaultConnection")
+
+            builder.UseSqlServer
+                ("Server=.;Database=FastFood;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new FastFoodContext(builder.Options);
+        }
+
+    }
+}
